1fd92834f9e9709494c6c0fdf82cbf6cb3ae3480
NourElHouda-Sarmout <2129@holbertonschool.com>
2020-11-05 20:53:39 +0200

    Fix betty style

--- 0x05-pathfinding/0-backtracking_array.c
+++ 0x05-pathfinding/0-backtracking_array.c
@@ -1,5 +1,4 @@
 #include "pathfinding.h"
-
 /**
  * map_create_delete - A function that creates/deletes a copy of 2d char array
  * @map: The pointer to a read-only two-dimensional array
@@ -10,27 +9,28 @@
  */
 char **map_create_delete(char **map, int rows, int cols, int create)
 {
-  int i = 0, j = 0;
-  char **map_cpy = NULL;
+int i = 0, j = 0;
+char **map_cpy = NULL;
 
-  if (create)
-    {
-      map_cpy = (char **)calloc(rows, sizeof(char *));
-      for (i = 0; i < rows && map_cpy; i++)
-	{
-	  map_cpy[i] = (char *)calloc(cols, sizeof(char));
-	  for (j = 0; j < cols && map_cpy[i]; j++)
-	    map_cpy[i][j] = map[i][j];
-	}
-    } else
-    {
-      for (i = 0; i < rows; i++)
-	{
-	  free(map[i]);
-	}
-      free(map);
-    }
-  return (map_cpy);
+if (create)
+{
+map_cpy = (char **)calloc(rows, sizeof(char *));
+for (i = 0; i < rows && map_cpy; i++)
+{
+map_cpy[i] = (char *)calloc(cols, sizeof(char));
+for (j = 0; j < cols && map_cpy[i]; j++)
+map_cpy[i][j] = map[i][j];
+}
+}
+else
+{
+for (i = 0; i < rows; i++)
+{
+free(map[i]);
+}
+free(map);
+}
+return (map_cpy);
 }
 
 /**
@@ -40,7 +40,7 @@ char **map_create_delete(char **map, int rows, int cols, int create)
  */
 void stack_push(queue_t *q, void *x)
 {
-  queue_push_back(q, x);
+queue_push_back(q, x);
 }
 
 /**
@@ -49,29 +49,29 @@ void stack_push(queue_t *q, void *x)
  */
 void stack_pop(queue_t *q1)
 {
-  void *top = NULL, *p = NULL;
-  queue_t *q2, temp;
+void *top = NULL, *p = NULL;
+queue_t *q2, temp;
 
-  if (q1)
-    {
-      q2 = queue_create();
-      top = dequeue(q1);
-      if (top)
-	p = dequeue(q1);
-      while (p)
-	{
-	  queue_push_back(q2, top);
-	  top = p;
-	  if (top)
-	    p = dequeue(q1);
-	}
-      free(top);
-      /*Swap the names of q1 and q2 */
-      temp = *q1;
-      *q1 = *q2;
-      *q2 = temp;
-      queue_delete(q2);
-    }
+if (q1)
+{
+q2 = queue_create();
+top = dequeue(q1);
+if (top)
+p = dequeue(q1);
+while (p)
+{
+queue_push_back(q2, top);
+top = p;
+if (top)
+p = dequeue(q1);
+}
+free(top);
+/*Swap the names of q1 and q2 */
+temp = *q1;
+*q1 = *q2;
+*q2 = temp;
+queue_delete(q2);
+}
 }
 
 /**
@@ -87,40 +87,40 @@ void stack_pop(queue_t *q1)
  */
 int findpath(char **map, int rows, int cols, queue_t *q, int x, int y)
 {
-  point_t *curr;
+point_t *curr;
 
-  if (x < 0 || y < 0 || x >= cols || y >= rows)
-    /*outside maze */
-    return (0);
-  if (map[y][x] == GOAL)
-    {
-      curr = malloc(sizeof(point_t));
-      curr->x = x;
-      curr->y = y;
-      /* reached goal */
-      stack_push(q, (void *)curr);
-      printf("Checking coordinates [%d, %d]\n", x, y);
-      return (1);
-    }
-  if (map[y][x] != OPEN && map[y][x] != START)
-    return (0);
-  printf("Checking coordinates [%d, %d]\n", x, y);
-  map[y][x] = VISITED;
-  curr = malloc(sizeof(point_t));
-  curr->x = x;
-  curr->y = y;
-  stack_push(q, (void *)curr);
-  if (findpath(map, rows, cols, q, x + 1, y))/*right */
-    return (1);
-  if (findpath(map, rows, cols, q, x, y + 1))/*below */
-    return (1);
-  if (findpath(map, rows, cols, q, x - 1, y))/*left */
-    return (1);
-  if (findpath(map, rows, cols, q, x, y - 1))/*top */
-    return (1);
-  stack_pop(q);
-  map[y][x] = BLOCKED;
-  return (0);
+if (x < 0 || y < 0 || x >= cols || y >= rows)
+/*outside maze */
+return (0);
+if (map[y][x] == GOAL)
+{
+curr = malloc(sizeof(point_t));
+curr->x = x;
+curr->y = y;
+/* reached goal */
+stack_push(q, (void *)curr);
+printf("Checking coordinates [%d, %d]\n", x, y);
+return (1);
+}
+if (map[y][x] != OPEN && map[y][x] != START)
+return (0);
+printf("Checking coordinates [%d, %d]\n", x, y);
+map[y][x] = VISITED;
+curr = malloc(sizeof(point_t));
+curr->x = x;
+curr->y = y;
+stack_push(q, (void *)curr);
+if (findpath(map, rows, cols, q, x + 1, y))/*right */
+return (1);
+if (findpath(map, rows, cols, q, x, y + 1))/*below */
+return (1);
+if (findpath(map, rows, cols, q, x - 1, y))/*left */
+return (1);
+if (findpath(map, rows, cols, q, x, y - 1))/*top */
+return (1);
+stack_pop(q);
+map[y][x] = BLOCKED;
+return (0);
 }
 
 /**
@@ -137,23 +137,23 @@ int findpath(char **map, int rows, int cols, queue_t *q, int x, int y)
 queue_t *backtracking_array(char **map, int rows, int cols,
 			    point_t const *start, point_t const *target)
 {
-  queue_t *q;
-  char **map_cpy;
+queue_t *q;
+char **map_cpy;
 
-  q = queue_create();
-  map_cpy = map_create_delete(map, rows, cols, 1);
-  if (map_cpy)
-    {
-      map_cpy[start->y][start->x] = START;
-      map_cpy[target->y][target->x] = GOAL;
-      findpath(map_cpy, rows, cols, q, start->x, start->y);
-      map_create_delete(map_cpy, rows, cols, 0);
-    }
-  if (!q->front)
-    {
-      free(q);
-      q = NULL;
-    }
+q = queue_create();
+map_cpy = map_create_delete(map, rows, cols, 1);
+if (map_cpy)
+{
+map_cpy[start->y][start->x] = START;
+map_cpy[target->y][target->x] = GOAL;
+findpath(map_cpy, rows, cols, q, start->x, start->y);
+map_create_delete(map_cpy, rows, cols, 0);
+}
+if (!q->front)
+{
+free(q);
+q = NULL;
+}
 
-  return (q);
+return (q);
 }
