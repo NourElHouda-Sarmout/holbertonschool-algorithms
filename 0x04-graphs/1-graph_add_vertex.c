df33028ed806326f34e1f66abea7e81bee02e15b
NourElHouda-Sarmout <2129@holbertonschool.com>
2020-11-17 01:47:33 +0200

    Fix errors

--- 0x04-graphs/1-graph_add_vertex.c
+++ 0x04-graphs/1-graph_add_vertex.c
@@ -1,60 +1,50 @@
 #include "graphs.h"
-#include <string.h>
-#include <stdio.h>
 
 /**
- * graph_add_vertex - add a vertex with @str data to the @graph
- *
- * @graph: graph to add vertex to
- * @str: data to put in vertex
- *
- * Return: pointer to vertex struct
- */
+* graph_add_vertex - add a vertex to a graph
+* @graph: pointer to a graph
+* @str: strin to store at the node
+* Store a copy of the string on the node. If the value
+* already exists, fail.
+* Return: pointer to created vertex or NULL on failure.
+*/
 vertex_t *graph_add_vertex(graph_t *graph, const char *str)
 {
-	vertex_t *vertex, *next_vertex;
+vertex_t *new, *tmp;
 
-	/* Create vertex */
-	if (graph == NULL || str == NULL)
-		return (NULL);
-	vertex = malloc(sizeof(vertex_t));
-	if (vertex == NULL)
-		return (NULL);
-	vertex->content = strdup(str);
-	if (vertex->content == NULL)
-		return (NULL);
-	vertex->nb_edges = 0;
-	vertex->edges = NULL;
-	vertex->next = NULL;
-	vertex->index = 0;
+if (!graph || !str)
+return (NULL);
 
-	/* Insert vertex */
-	if (graph->vertices == NULL)
-		graph->vertices = vertex;
-	else
-	{
-		next_vertex = graph->vertices;
-		vertex->index = 1;
-		while (next_vertex->next != NULL)
-		{
-			if (!strcmp(next_vertex->content, str))
-			{
-				free(vertex->content);
-				free(vertex);
-				return (NULL);
-			}
-			next_vertex = next_vertex->next;
-			vertex->index++;
-		}
-		if (!strcmp(next_vertex->content, str))
-		{
-			free(vertex->content);
-			free(vertex);
-			return (NULL);
-		}
-		next_vertex->next = vertex;
-	}
-	graph->nb_vertices++;
+tmp = graph->vertices;
+/* loop to the last-1 element in list */
+while (tmp && tmp->next)
+{
+if (!strcmp(tmp->content, str))
+return (NULL);
+tmp = tmp->next;
+}
+/* last element */
+if (tmp && !strcmp(tmp->content, str))
+return (NULL);
+/* create a new vertex */
+new = malloc(sizeof(*new));
+if (!new)
+return (NULL);
+new->content = strdup(str);
+if (!new->content)
+{
+free(new);
+return (NULL);
+}
 
-	return (vertex);
+new->index = graph->nb_vertices++;
+new->nb_edges = 0;
+new->edges = 0;
+new->next = NULL;
+if (tmp)
+tmp->next = new;
+else
+graph->vertices = new;
+
+return (new);
 }
